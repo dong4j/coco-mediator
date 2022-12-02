dc9d41a25775bba8790a965f4491e2d7f021e075
dong4j <dong4j@gmail.com>
2022-12-02 22:58:41 +0800

    style: 更新版权

--- coco-mediator-server/src/main/java/io/github/dong4j/coco/mediator/MediatorServer.java
+++ coco-mediator-server/src/main/java/io/github/dong4j/coco/mediator/MediatorServer.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) $year dong4j
+ * Copyright (c)  2022. dong4j
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
